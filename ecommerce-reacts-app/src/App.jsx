94f6f4cdd91f2d35f57fbf5b0863f4548136ea31
kstmostofa <mostofa122@gmail.com>
2025-01-25 02:07:24 +0600

    add react app

---
+++ ecommerce-reacts-app/src/App.jsx
@@ -0,0 +1,40 @@
+
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+import AboutUs from './AboutUs';
+
+function App() {
+  
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  return (
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+          <div className="landing_content">
+            <h1>Yang Plant Nursery</h1>
+            <div className="divider"></div>
+            <p>Breathe Green 🌱</p>          
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+            Our Plants
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+
